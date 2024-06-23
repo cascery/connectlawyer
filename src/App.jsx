80646284f1f1cfb9c3d7d86e6517a5eca6cba198
cassy <136271194+cascery@users.noreply.github.com>
2024-06-23 17:42:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,38 @@ function App() {
 
 
   return (
-   <h1>hiiiiii</h1> 
+ <Router>
+      {/* Conditional rendering of Navbar based on user type */}
+      {isLoggedIn && (userType === 'client' ? <NavbarClient /> : <Navbar />)}      
+      <Routes>
+                <Route path="/" exact element={<Mainpage />} />
+
+      <Route path="/reunioNcli" exact element ={<AcceptedRequests/>}/>
+
+        <Route path="/notification" exact element ={<ForumNotificationModal/>}/>
+      <Route path="/editprofilecli" exact element={<EditProfileClient />} />
+      <Route path="/managefiles" exact element={<ManageFiles />} />
+
+        <Route path="/reunions" exact element={<Reunions />} />
+        <Route path="/search" exact element={<Searchforlawyers />} />
+        <Route path="/announcement" exact element={<Announcements />} />
+        <Route path="/editprofile" exact element={<EditProfile />} />
+        <Route path="/postannouncement" exact element={<PostAnn />} />
+        <Route path="/requests" exact element={<RequestsTab />} />
+        <Route path="/fetchdocs" exact element={<FetchDocuments />} />
+        <Route path="/profile" exact element={<SelfProfileLawyer />} />
+        <Route path="/askingplace" exact element={<AskingPlace />} />
+        <Route path="/feed" exact element={<Feed />} />
+        <Route path="/editprofilelawyer" exact element={<Editprofilelawyer />} />
+        <Route path="/configure-request/:serviceRequestId" element={<ConfigureRequest />} />
+        <Route path="/loginclient" exact element={<Loginclient />} />
+        <Route path="/signupclient" exact element={<FirstPage />} />
+        <Route path="/signuplawyer" exact element={<Signuplawyer />} />
+        <Route path="/loginlawyer" exact element={<Loginlawyer />} />
+        <Route path="/lawyers/:id" element={<LawyerProfile />} />
+        <Route path="/feed/:id" element={<Forum />} />
+      </Routes>
+    </Router>
   
   );
 }
